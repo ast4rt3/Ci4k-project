0696703aa80ffdb0e5ab62c212ec7b258deb1042
Lawrence <53902954+ast4rt3@users.noreply.github.com>
2024-11-17 18:33:27 +0800

    fffffffff

--- signalingServer.js
+++ signalingServer.js
@@ -1,5 +1,4 @@
 const WebSocket = require('ws');
-<<<<<<< HEAD
 
 // Create WebSocket server
 const wss = new WebSocket.Server({ port: 8080 });
@@ -26,29 +25,3 @@ wss.on('connection', (ws) => {
 });
 
 console.log('Signaling server running on ws://localhost:8080');
-=======
-const wss = new WebSocket.Server({ port: 8080 });
-
-let clients = [];
-
-wss.on('connection', (ws) => {
-  // Add the new client connection
-  clients.push(ws);
-
-  // When a message is received, forward it to the other client
-  ws.on('message', (message) => {
-    clients.forEach((client) => {
-      if (client !== ws) {
-        client.send(message);
-      }
-    });
-  });
-
-  ws.on('close', () => {
-    // Remove disconnected client
-    clients = clients.filter((client) => client !== ws);
-  });
-});
-
-console.log('Signaling server is running on ws://localhost:8080');
->>>>>>> parent of 8800231 (client/admin added)
