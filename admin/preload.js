311fd4a5578f783512790126b71c23cc6b456e8c
Lawrence <53902954+ast4rt3@users.noreply.github.com>
2024-12-18 12:11:06 +0800

    RESET

--- admin/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    onClientData: (callback) => ipcRenderer.on('update-client-list', (event, data) => callback(data)),
-    onStatusUpdate: (callback) => ipcRenderer.on('status-update', (event, data) => callback(data)),
-    onClientActionMessage: (callback) => ipcRenderer.on('client-action-message', (event, data) => callback(data)),
-    reconnect: () => ipcRenderer.send('reconnect'),
-});
